b5b6a004262b1267c029d4e13463d371170e7348
RahulDivyanja <128392401+RahulDivyanja@users.noreply.github.com>
2023-07-28 00:05:54 +0530

    Add files via upload

--- Project.py
+++ Project.py
@@ -1,14 +1,34 @@
 import tkinter as tk
 import threading
-import datetime
+import time
+#from win10toast import ToastNotifier
+from winotify import Notification,audio
+
+#toaster=ToastNotifier()
+
 root = tk.Tk()
+root.title('Eye Rest Reminder')
+my_label = tk.Label(root,text='')
+my_label.pack(pady=20)
+
+def clock():
+    hour = time.strftime("%H")
+    minute = time.strftime('%M')
+    second =time.strftime('%S')
+    my_label.config(text=f"{hour}:{minute}:{second}")
+    my_label.after(1000,clock)
 
 def eye_rest_reminder():
-    print("It's time to take a break and rest your eyes!")
-def start_timer():
-    timer = threading.Timer(30, eye_rest_reminder)
-    timer.start()
-start_timer()
+    #toaster.show_toast("Eye rest reminder","Now is the time to rest your eyes",duration=5)
+    toast = Notification(app_id='Take rest',title='Eye Rest Reminder',msg='Now is the time to rest your eyes',duration='long')
+    toast.set_audio(audio.LoopingAlarm,loop=True)
+    toast.show()
 
+def start_timer():
+    timer = threading.Timer(5, eye_rest_reminder)
+    timer.start()
+
+start_timer()
+clock()
 
 root.mainloop()
